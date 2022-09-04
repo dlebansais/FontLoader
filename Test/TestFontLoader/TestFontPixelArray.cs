31148f1f1d965734036fcd9df4c2fd1f2dbd5748
dlebansais <dlebansais@gmail.com>
2022-09-04 18:12:25 +0200

    Misc fixes.

--- Test/TestFontLoader/TestFontPixelArray.cs
+++ Test/TestFontLoader/TestFontPixelArray.cs
@@ -13,13 +13,28 @@ public class TestFontPixelArray
     public void BasicTest()
     {
         Assembly TestAssembly = Assembly.GetExecutingAssembly();
-        Stream TestBitmapStream = TestAssembly.GetManifestResourceStream($"{typeof(TestFontPixelArray).Namespace}.FakeResource.png");
+        Stream TestBitmapStream = TestAssembly.GetManifestResourceStream($"{typeof(TestFontPixelArray).Namespace}.Black.png");
         Bitmap TestBitmap = new Bitmap(TestBitmapStream);
-
         FontPixelArray TestPixelArray = FontPixelArray.FromBitmap(TestBitmap);
-        TestPixelArray.DebugPrint();
+    }
 
-        FontPixelArray TestClipped = TestPixelArray.Clipped();
-        Assert.IsTrue(TestClipped.IsClipped);
+    [Test]
+    public void ClippedTest()
+    {
+        Assembly TestAssembly = Assembly.GetExecutingAssembly();
+
+        Stream BlackBitmapStream = TestAssembly.GetManifestResourceStream($"{typeof(TestFontPixelArray).Namespace}.Black.png");
+        Bitmap BlackBitmap = new Bitmap(BlackBitmapStream);
+        FontPixelArray BlackPixelArray = FontPixelArray.FromBitmap(BlackBitmap);
+        BlackPixelArray.DebugPrint();
+        FontPixelArray BlackClipped = BlackPixelArray.Clipped();
+        Assert.IsTrue(BlackClipped.IsClipped);
+
+        Stream WhiteBitmapStream = TestAssembly.GetManifestResourceStream($"{typeof(TestFontPixelArray).Namespace}.White.png");
+        Bitmap WhiteBitmap = new Bitmap(WhiteBitmapStream);
+        FontPixelArray WhitePixelArray = FontPixelArray.FromBitmap(WhiteBitmap);
+        WhitePixelArray.DebugPrint();
+        FontPixelArray WhiteClipped = WhitePixelArray.Clipped();
+        Assert.AreEqual(FontPixelArray.Empty, WhiteClipped);
     }
 }
