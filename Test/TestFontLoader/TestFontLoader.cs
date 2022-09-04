08724e7d7c9646d08839785b0478cf91240ba9d0
dlebansais <dlebansais@gmail.com>
2022-09-04 14:27:29 +0200

    Cleanup.

--- Test/TestFontLoader/TestFontLoader.cs
+++ Test/TestFontLoader/TestFontLoader.cs
@@ -2,10 +2,6 @@
 
 using FontLoader;
 using NUnit.Framework;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Reflection;
 
 [TestFixture]
 public class TestFontLoader
